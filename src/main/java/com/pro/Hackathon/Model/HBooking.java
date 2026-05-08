4b8700e5b6f44026a4592d6cb7626f78ef7899aa
AREDDY15 <amulyareddypasham15@gmail.com>
2026-05-08 13:55:13 +0530

    ready to run

--- src/main/java/com/pro/Hackathon/Model/HBooking.java
+++ src/main/java/com/pro/Hackathon/Model/HBooking.java
@@ -5,12 +5,13 @@ import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+
 
 public class HBooking {
 	
