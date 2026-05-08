a3431b788704a4b63007a0fcacf5d2ae859109cd
name <pettamaksharavarma@gmail.com>
2026-05-08 13:52:36 +0530

    modified repo queries

--- src/main/java/com/pro/Hackathon/Model/HBooking.java
+++ src/main/java/com/pro/Hackathon/Model/HBooking.java
@@ -5,13 +5,13 @@ import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-
+@Entity
 public class HBooking {
 	
 	@Id
