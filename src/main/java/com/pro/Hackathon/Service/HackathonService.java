6094208a79f994a857bd9074367bf9d57350370b
AREDDY15 <amulyareddypasham15@gmail.com>
2026-05-08 12:37:16 +0530

    service change

--- src/main/java/com/pro/Hackathon/Service/HackathonService.java
+++ src/main/java/com/pro/Hackathon/Service/HackathonService.java
@@ -22,7 +22,7 @@ import com.pro.Hackathon.Repository.HUserRepository;
 
 
 @Service
-public class HotelService {
+public class HackathonService {
 
     @Autowired
     private HUserRepository userRepo;
